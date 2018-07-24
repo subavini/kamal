722b596f351773381cbf3522526d09d880f5ff6e
subavini <subashinisuba.v@gmail.com>
2018-07-24 11:56:09 +0530

    Create nextnum.c

---
+++ beginnerlevel/nextnum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+   int n;
+   scanf("%d",&n);
+   if(n>=0)
+   {
+       n=n+1;
+       printf("%d",n);
+   }
+   else{
+       printf("invalid");
+   }
+   return 0;
+}
