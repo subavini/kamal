f8166d2b6d90cd4e6dda385306d83057a51c2268
subavini <subashinisuba.v@gmail.com>
2018-07-24 11:22:50 +0530

    Create range.c

---
+++ beginnerlevel/range.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("\nenter num");
+    scanf("%d",&n);
+    if(n>=1&&n<=9)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
